eb8359ce388dcb0c4c57c88ff7f8b1114c292748
Nicolas Rigal <nrigal@insa-toulouse.fr>
2022-11-07 12:13:22 +0100

    drivers ecoute

--- MesDrivers/Source/Ecoute.c
+++ MesDrivers/Source/Ecoute.c
@@ -1,10 +1,19 @@
 #include "Ecoute.h"
 #include "MyTimer.h"
 
-//TODO : configurer PWM voiles (timer, init....) TIM2 CH2
+//TODO : fichiers voiles.c/.h avec Ecoute_InitPWM() et  Ecoute_BorderEcoute(Girouette_GetAngle())
+MyTimer_Struct_TypeDef Timer = {TIM2 , 5999 , 5999} ;
+	
+void Ecoute_InitPWM()
+{
+	MyTimer_Base_Init (&Timer) ;
+	MyTimer_PWM (Timer.Timer, 2) ;
+	MyTimer_PWM_ToGPIO (Timer.Timer , 2) ;
+	MyTimer_Base_Start (Timer.Timer) ;
+}
 
 void Ecoute_BorderEcoute(float angle)
 {
 	float ratio = angle/360 ;
-	
+	MyTimer_PWM_Cycle (Timer.Timer, 2, ratio) ;
 }
