6357d3ea4ae455b1047bcd499cd080cd3f7c6d74
Celiadespaux <celiadespaux@gmail.com>
2022-11-08 17:35:16 +0100

    UART fini

--- MesDrivers/Source/UART.c
+++ MesDrivers/Source/UART.c
@@ -1,13 +1,11 @@
-#include "stm32f10x.h"
-#include "Mytimer.h"
-#include "MyGPIO.h"
+
 #include "UART.h"
 
 // txe  rxne  rxneie
-int recu ;
-//interruption dr=recu 
-
+signed char Octet;
 USART_TypeDef * UART;
+
+void (*handlerUART)(char);
 
 void UART_Config(USART_TypeDef * UART){
 
@@ -29,34 +27,56 @@ void UART_Config(USART_TypeDef * UART){
 	
 	UART->CR1 &= ~(0x1 << 12); // UART->CR1 = (10<<USART_CR1_M) ;
 	
+	UART->CR1 |= (0x1 <<13);  //UART->CR1 = USART_CR1_UE ;s
+	
 	// -> on met à 0 les bits STOP (12 et 13) de CR2
 	UART->CR2 &= ~((0x1<< 12) | (0x1 << 13));
 	
 	UART->BRR = 3750;
 	
+	UART->CR1 |= (0x1 << 3);  //UART->CR1 = USART_CR1_TE; enable transmission
+	
+	UART->SR |= USART_SR_TC ;
+	
+	UART->CR1 |= USART_CR1_RE; //enable reception //UART->CR1 |= (0x1<<2) ;
+	
+	UART->CR1 |= USART_CR1_RXNEIE ;
 	// generate interuption USART_CR1 |= (1<<USART_CR1_RXNEIE);
-	
-	UART->CR1 = USART_CR1_RE; //enable reception
-	UART->CR1 = USART_CR1_TE; //enable transmission
+}
 
-	UART->CR1 = USART_CR1_UE ; //UART->CR1 |= (0x1 <<13);
+void UART_send_char(char caractere){
+	while(USART_SR_TC==0);
+	UART->DR = caractere;
+}
+
+void UART_send_message(const char message[]){
+	int i = 0 ;
+	do {
+		UART_send_char(message[i]);
+		i++ ;
+		} while (message[i]!='\0') ;
+}
+
+
+void UART_confIT(void (* ptrFonction) (char)){
 	
-	//aller voir les fonctionalites de TE (et pourquoi le mettre a 1)
-	//pareil pour tc
+	handlerUART = ptrFonction ;
+	
+	NVIC_EnableIRQ(USART3_IRQn);
+	NVIC_SetPriority(USART3_IRQn, 10);
+	
 	
 }
 
-/*
-1. Enable the USART by writing the UE bit in USART_CR1 register to 1.
-2. Program the M bit in USART_CR1 to define the word length.
-3. Program the number of stop bits in USART_CR2.
-4. Select DMA enable (DMAT) in USART_CR3 if Multi buffer Communication is to take
-place. Configure the DMA register as explained in multibuffer communication.
-5. Select the desired baud rate = 50 using the USART_BRR register.
-6. Set the TE bit in USART_CR1 to send an idle frame as first transmission.
-7. Write the data to send in the USART_DR register (this clears the TXE bit). Repeat this
-for each data to be transmitted in case of single buffer.
-8. After writing the last data into the USART_DR register, wait until TC=1. This indicates
-that the transmission of the last frame is complete. This is required for instance when
-the USART is disabled or enters the Halt mode to avoid corrupting the last
-transmission.*/
+void USART3_IRQHandler(){
+
+		Octet=(USART3->DR) ;
+		USART3->SR &= ~USART_SR_RXNE; // rxne = 0
+
+}
+
+  
+
+
+
+
--- Projet_Voilier/Sources/main.c
+++ Projet_Voilier/Sources/main.c
@@ -4,9 +4,16 @@
 #include "MyTimer.h"
 #include "UART.h"
 
+void fonctionn(char caractere){
+		
+	}
+
 int main (void)
 {
+	
 UART_Config(USART3);
+UART_confIT(&fonctionn);
+	
 	
 	while (1) {
 	}
