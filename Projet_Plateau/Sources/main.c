43ba80fb896ebbc8cf3d3030e38661ce8fcd44ca
Celiadespaux <celiadespaux@gmail.com>
2022-11-21 11:06:57 +0100

    ajout systick

--- Projet_Plateau/Sources/main.c
+++ Projet_Plateau/Sources/main.c
@@ -5,17 +5,27 @@
 #include "UART.h"
 #include "Plateau.h"
 
-
+void SysTick_Handler(){
+Plateau_set_vitesse(UART_getOctet());
+}
 
 int main (void)
 {
-
+	SysTick->CTRL |= SysTick_CTRL_ENABLE;
+	SysTick->CTRL |= SysTick_CTRL_CLKSOURCE; 
+	SysTick->CTRL |= SysTick_CTRL_TICKINT; 
+	//SysTick->CTRL = 7 ;
+	
+	SysTick->LOAD = 14400000 ;
+	NVIC_EnableIRQ(SysTick_IRQn);
+	NVIC_SetPriority(SysTick_IRQn, 10);
+	
+	
 	Plateau_init();
 	UART_Config(USART3);
 
 	
 	while(1){
-	Plateau_set_vitesse(UART_getOctet());
 	Plateau_Tourner();
 	}
 }
