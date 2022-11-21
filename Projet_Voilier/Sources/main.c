40ce4e77669ebf5fb0b752806df85f37da1d16d7
Nicolas Rigal <nrigal@insa-toulouse.fr>
2022-11-21 11:22:00 +0100

    resolution conflits

--- Projet_Voilier/Sources/main.c
+++ Projet_Voilier/Sources/main.c
@@ -2,13 +2,30 @@
 #include "stm32f10x.h"
 #include "Voiles.h"
 #include "Plateau.h"
+#include "UART.h"
+
+void SysTick_Handler(){
+	Plateau_set_vitesse(UART_getOctet());
+	Voiles_Update() ;
+}
 
 int main (void)
 {
 	
 	Voiles_Init() ;
+	Plateau_init();
+	UART_Config(USART3);
+	
+	SysTick->CTRL |= SysTick_CTRL_ENABLE;
+	SysTick->CTRL |= SysTick_CTRL_CLKSOURCE; 
+	SysTick->CTRL |= SysTick_CTRL_TICKINT; 
+	//SysTick->CTRL = 7 ;
+	
+	SysTick->LOAD = 14400000 ;
+	NVIC_EnableIRQ(SysTick_IRQn);
+	NVIC_SetPriority(SysTick_IRQn, 10);
 	
 	while (1) {
-		Voiles_Update() ;
+		Plateau_Tourner();
 	}
 }
